a4f825311f35360e461123895beeab14c0802935
Anton Robert <74978336+UnX-Robert@users.noreply.github.com>
2022-01-19 19:05:20 +0200

    Update app.js

--- app.js
+++ app.js
@@ -59,7 +59,7 @@ passport.deserializeUser(function(id, done) {
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: 'http://localhost:3000/auth/google/secrets',
+    callbackURL: 'https://secure-badlands-03579.herokuapp.com/auth/google/secrets',
     userProfileURL: 'https://www.googleapis.com/oauth2/v3/userinfo'
   },
   function(accessToken, refreshToken, profile, cb) {
